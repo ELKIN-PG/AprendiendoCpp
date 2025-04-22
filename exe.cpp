fc0abadbc08eb45f03676aac3d0925a39ec19150
ElkinJS <perezgilelkinalbert@gmail.com>
2025-04-21 23:31:21 -0500

    master - Primer Commit - Agregando Primeros Scripts

---
+++ exe.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+
+int main(){
+    cout<<"Hola mundo";
+    return 0;
+}
---
+++ exe4.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int num = 10;
+    int num2 = 20;
+    printf("Hola mundo %d y el otro numero es %d", num, num2);
+    return 0;
+}
